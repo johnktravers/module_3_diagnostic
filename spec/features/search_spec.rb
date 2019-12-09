f133c7488143334e18d0ef6ae2bd179b2c268721
John Travers <johnk.travers@gmail.com>
2019-12-09 10:21:17 -0700

    Fix typo and small differences in page content

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -6,16 +6,16 @@ RSpec.describe 'Order of the Phoenix Member Search' do
     visit root_path
 
     select 'Gryffindor', from: :house
-    click_button 'Search for Members'
+    click_button 'Search For Members'
 
     expect(current_path).to eq(search_path)
-    expect(page).to have_content('21 Order of the Phoenix Members in House Gryffindor')
+    expect(page).to have_content('21 Members in House Gryffindor')
 
     expect(page).to have_css('.member', count: 21)
 
     within(first('.member')) do
       expect(first('.name').text).to_not be_empty
-      expect(first('.house').text).to eq('Gryffindor')
+      expect(first('.house').text).to eq('House: Gryffindor')
       expect(page).to have_css('.role')
       expect(page).to have_css('.patronus')
     end
